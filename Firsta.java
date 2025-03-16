7b5ce5e766aec2be3756f8080a080e45014d1edd
MMODAK2025 <mamatamodak2026@gmail.com>
2025-03-16 19:49:43 +0530

    Add files via upload

---
+++ Firsta.java
@@ -0,0 +1,10 @@
+public class Firsta {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        System.out.print("Mamata is the best");
+        System.out.print("Mamata Mam");
+        System.out.print("I LOVE MY INDIA");
+        
+    }
+}
